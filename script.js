44e843fe1c787fae5152e21248dfcdcb7508ff9f
Sikhosana24 <ambrosiasikhosana@gmail.com>
2024-08-30 11:18:30 +0200

    Implemented pet adoption detail validation with regex

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function validateSyntax() {
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
 
+    const pattern = /^pet_[a-zA-Z0-9]+$/;
+
+    if (pattern.test(input)) {
+        result = "valid syntax";
+    } else{
+        result ="Invalid Syntax"
+    }
+
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
